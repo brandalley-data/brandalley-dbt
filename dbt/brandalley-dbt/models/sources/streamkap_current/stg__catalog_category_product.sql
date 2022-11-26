a3f59dac10f0dc22a22ca176ac48f78fae26e381
joshua olufemi <Joshua.smart.olufemi@brandalley.co.uk>
2022-11-26 10:45:38 +0000

    composite key added to two models: stg__catalog_category_product and stg__eav_attribute_option_value model, along with two unique tests on each composite key that were run and passed well.

--- dbt/brandalley-dbt/models/sources/streamkap_current/stg__catalog_category_product.sql
+++ dbt/brandalley-dbt/models/sources/streamkap_current/stg__catalog_category_product.sql
@@ -1,8 +1,25 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = ['category_id', 'product_id']
+    unique_key = 'composite_key'
 ) }}
+
+
+with streamkap_source as (
+
 {{ streamkap_incremental_on_source_to_current(
     source_name = 'catalog_category_product',
     id_field = config.get('unique_key')
 ) }}
+
+)
+
+, table_cte as (
+
+SELECT 
+{{ dbt_utils.surrogate_key(['category_id', 'product_id']) }} as composite_key
+, *   
+FROM streamkap_source 
+
+) 
+
+SELECT * FROM table_cte 
--- dbt/brandalley-dbt/models/sources/streamkap_current/stg__eav_attribute_option_value.sql
+++ dbt/brandalley-dbt/models/sources/streamkap_current/stg__eav_attribute_option_value.sql
@@ -1,10 +1,25 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = ['option_id', 'store_id'],
-    cluster_by = ['option_id', 'store_id'],
+    unique_key = 'composite_key',
+    cluster_by = 'composite_key'
 ) }}
+
+
+with streamkap_source as (
 {{ streamkap_incremental_on_source_to_current(
     source_name = 'eav_attribute_option_value',
     id_field = config.get('unique_key'),
     order_offset_field = 'value_id'
 ) }}
+
+)
+
+, table_cte as (
+SELECT 
+{{ dbt_utils.surrogate_key(['option_id', 'store_id']) }} as composite_key
+, *
+FROM streamkap_source 
+
+)
+
+SELECT * FROM table_cte
